a06c2260b638b92cf1cf0742dada9a799686f3de
Farcik <farcik.s.r.o@gmail.com>
2026-05-31 11:09:42 +0200

    Add logging for guild member addition event
    Log member addition to the console when a new member joins the guild.

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -8,6 +8,11 @@ import { getServerCounters, updateCounter } from '../services/serverstatsService
 import { setBirthday as dbSetBirthday } from '../utils/database.js';
 import { logger } from '../utils/logger.js';
 
+async execute(member) {
+    console.log("GUILD MEMBER ADD FIRED:", member.user.tag);
+
+    try {
+      
 export default {
   name: Events.GuildMemberAdd,
   once: false,
